8aedc5d16a09674a8e450dbef95b633522c782ea
richardodliu <richardodliu@gmail.com>
2025-05-02 18:36:45 +0800

    update

--- OpenCodeEval/eval/func_eval.py
+++ OpenCodeEval/eval/func_eval.py
@@ -53,7 +53,7 @@ def check_correctness(task_id: int,
             check_program = solution
 
             try:
-                exec_globals = {}
+                exec_globals = {'__builtins__': __builtins__}
                 with swallow_io():
                     with time_limit(time_out):
                         exec(check_program, exec_globals)
